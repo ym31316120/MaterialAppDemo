d8bf7ceea8fa4a4d33d6d5c264ebb6695bf55d6a
Magee_Yang <ym31316120@gmail.com>
2015-11-05 11:25:52 +0800

    1

--- MaterialAppDemo/js/app.js
+++ MaterialAppDemo/js/app.js
@@ -19,6 +19,14 @@ function($routeProvider) {
         controller : 'ListCtrl'
     }).when('/list/autocomplete', {
         templateUrl : 'pages/autocomplete.html',
+        controller : 'autoCompleteCtrl',
+        resolve:{
+            'ButtonServices':['$route',function($route){
+                
+            }]
+        }
+    }).when('/list/button', {
+        templateUrl : 'pages/button.html',
         controller : 'ButtonCtrl',
         resolve:{
             'ButtonServices':['$route',function($route){
@@ -33,12 +41,35 @@ function($routeProvider) {
                 
             }]
         }
+    }).when('/list/card', {
+        templateUrl : 'pages/card.html',
+        controller : 'cardCtrl',
+        resolve:{
+            'ButtonServices':['$route',function($route){
+                
+            }]
+        }
+    }).when('/list/datePicker', {
+        templateUrl : 'pages/datepicker.html',
+        controller : 'datePickerCtrl',
+        resolve:{
+            'ButtonServices':['$route',function($route){
+                
+            }]
+        }
     }).otherwise({
         redirectTo : '/'
     });
 }]);
 
-
+app.controller('cardCtrl',function($scope){
+    $scope.imagePath = 'img/washedout.png';
+    $scope.title = 'card';
+    $scope.gobackhy = function(event){
+        console.log("goback");
+        history.back();
+    };
+});
 
 app.controller('ListCtrl', function($scope,$location, $mdDialog) {
 
@@ -56,12 +87,12 @@ app.controller('ListCtrl', function($scope,$location, $mdDialog) {
         name : 'Button',
         img : 'img/angular-logo.svg',
         newMessage : true,
-        url : '#'
+        url : '/list/button'
     }, {
         name : 'Card',
         img : 'img/angular-logo.svg',
         newMessage : true,
-        url : '#'
+        url : '/list/card'
     }, {
         name : 'CheckBox',
         img : 'img/angular-logo.svg',
@@ -81,7 +112,7 @@ app.controller('ListCtrl', function($scope,$location, $mdDialog) {
         name : 'Datepicker',
         img : 'img/angular-logo.svg',
         newMessage : true,
-        url : '#'
+        url : '/list/datePicker'
     }, {
         name : 'Dialog',
         img : 'img/angular-logo.svg',
@@ -323,3 +354,93 @@ app.config(function($mdIconProvider) {
       $http.get(url, {cache: $templateCache});
     });
   });
+  
+  
+
+app.controller('autoCompleteCtrl', DemoCtrl);
+  function DemoCtrl ($timeout, $q, $log,$scope) {
+    $scope.title="autoComplete";
+    $scope.gobackhy = function(event){
+        history.back();
+    };
+    var self = this;
+    self.simulateQuery = false;
+    self.isDisabled    = false;
+    // list of `state` value/display objects
+    self.states        = loadAll();
+    self.querySearch   = querySearch;
+    self.selectedItemChange = selectedItemChange;
+    self.searchTextChange   = searchTextChange;
+    self.newState = newState;
+    function newState(state) {
+      alert("Sorry! You'll need to create a Constituion for " + state + " first!");
+    };
+    // ******************************
+    // Internal methods
+    // ******************************
+    /**
+     * Search for states... use $timeout to simulate
+     * remote dataservice call.
+     */
+    function querySearch (query) {
+      var results = query ? self.states.filter( createFilterFor(query) ) : self.states,
+          deferred;
+      if (self.simulateQuery) {
+        deferred = $q.defer();
+        $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
+        return deferred.promise;
+      } else {
+        return results;
+      }
+    };
+    function searchTextChange(text) {
+      $log.info('Text changed to ' + text);
+    };
+    function selectedItemChange(item) {
+      $log.info('Item changed to ' + JSON.stringify(item));
+    };
+    /**
+     * Build `states` list of key/value pairs
+     */
+    function loadAll() {
+      var allStates = 'Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware,\
+              Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana,\
+              Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana,\
+              Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina,\
+              North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina,\
+              South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia,\
+              Wisconsin, Wyoming';
+      return allStates.split(/, +/g).map( function (state) {
+        return {
+          value: state.toLowerCase(),
+          display: state
+        };
+      });
+    };
+    /**
+     * Create filter function for a query string
+     */
+    function createFilterFor(query) {
+      var lowercaseQuery = angular.lowercase(query);
+      return function filterFn(state) {
+        return (state.value.indexOf(lowercaseQuery) === 0);
+      };
+    };
+  };
+app.controller('datePickerCtrl', function($scope) {
+  $scope.myDate = new Date();
+   $scope.title = 'datePicker';
+    $scope.gobackhy = function(event){
+        console.log("goback");
+        history.back();
+    };
+  $scope.minDate = new Date(
+      $scope.myDate.getFullYear(),
+      $scope.myDate.getMonth() - 2,
+      $scope.myDate.getDate());
+  $scope.maxDate = new Date(
+      $scope.myDate.getFullYear(),
+      $scope.myDate.getMonth() + 2,
+      $scope.myDate.getDate());
+      
+});
