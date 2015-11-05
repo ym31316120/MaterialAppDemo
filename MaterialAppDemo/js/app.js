c9a8a4e6045325d8452414f464a417579889c0ad
Magee_Yang <ym31316120@gmail.com>
2015-11-05 10:41:04 +0800

    add

--- MaterialAppDemo/js/app.js
+++ MaterialAppDemo/js/app.js
@@ -19,7 +19,20 @@ function($routeProvider) {
         controller : 'ListCtrl'
     }).when('/list/autocomplete', {
         templateUrl : 'pages/autocomplete.html',
-        controller : 'ButtonCtrl'
+        controller : 'ButtonCtrl',
+        resolve:{
+            'ButtonServices':['$route',function($route){
+                
+            }]
+        }
+    }).when('/list/bottomsheet', {
+        templateUrl : 'pages/bottomsheet.html',
+        controller : 'BottomSheetExample',
+        resolve:{
+            'ButtonServices':['$route',function($route){
+                
+            }]
+        }
     }).otherwise({
         redirectTo : '/'
     });
@@ -38,7 +51,7 @@ app.controller('ListCtrl', function($scope,$location, $mdDialog) {
         name : 'Bottom Sheet',
         img : 'img/angular-logo.svg',
         newMessage : true,
-        url : '#'
+        url : '/list/bottomsheet'
     }, {
         name : 'Button',
         img : 'img/angular-logo.svg',
@@ -219,3 +232,94 @@ app.controller('ListCtrl', function($scope,$location, $mdDialog) {
         $mdDialog.show($mdDialog.alert().title('Secondary Action').content('Secondary actions can be used for one click actions').ariaLabel('Secondary click demo').ok('Neat!').targetEvent(event));
     };
 });
+
+app.config(function($mdIconProvider) {
+    $mdIconProvider
+      .icon('share-arrow', 'img/icons/share-arrow.svg', 24)
+      .icon('upload', 'img/icons/upload.svg', 24)
+      .icon('copy', 'img/icons/copy.svg', 24)
+      .icon('print', 'img/icons/print.svg', 24)
+      .icon('hangout', 'img/icons/hangout.svg', 24)
+      .icon('mail', 'img/icons/mail.svg', 24)
+      .icon('message', 'img/icons/message.svg', 24)
+      .icon('copy2', 'img/icons/copy2.svg', 24)
+      .icon('facebook', 'img/icons/facebook.svg', 24)
+      .icon('twitter', 'img/icons/twitter.svg', 24);
+  })
+.controller('BottomSheetExample', function($scope, $timeout, $mdBottomSheet, $mdToast) {
+  $scope.alert = '';
+  $scope.title = "BottomSheet";
+  $scope.gobackhy = function(event){
+        console.log("goback");
+        history.back();
+    };
+  $scope.showListBottomSheet = function($event) {
+    $scope.alert = '';
+    $mdBottomSheet.show({
+      templateUrl: 'pages/bottom-sheet-list-template.html',
+      controller: 'ListBottomSheetCtrl',
+      targetEvent: $event
+    }).then(function(clickedItem) {
+      $scope.alert = clickedItem['name'] + ' clicked232!';
+    });
+  };
+  $scope.showGridBottomSheet = function($event) {
+    $scope.alert = '';
+    $mdBottomSheet.show({
+      templateUrl: 'pages/bottom-sheet-grid-template.html',
+      controller: 'GridBottomSheetCtrl',
+      clickOutsideToClose: false,
+      targetEvent: $event
+    }).then(function(clickedItem) {
+      $mdToast.show(
+            $mdToast.simple()
+              .content(clickedItem['name'] + ' clicked23!')
+              .position('top right')
+              .hideDelay(1500)
+          );
+    });
+  };
+})
+.controller('ListBottomSheetCtrl', function($scope, $mdBottomSheet) {
+  $scope.items = [
+    { name: 'Share', icon: 'share-arrow' },
+    { name: 'Upload', icon: 'upload' },
+    { name: 'Copy', icon: 'copy' },
+    { name: 'Print this page', icon: 'print' },
+  ];
+  $scope.listItemClick = function($index) {
+    var clickedItem = $scope.items[$index];
+    $mdBottomSheet.hide(clickedItem);
+  };
+})
+.controller('GridBottomSheetCtrl', function($scope, $mdBottomSheet) {
+  $scope.items = [
+    { name: 'Hangout', icon: 'hangout' },
+    { name: 'Mail', icon: 'mail' },
+    { name: 'Message', icon: 'message' },
+    { name: 'Copy', icon: 'copy2' },
+    { name: 'Facebook', icon: 'facebook' },
+    { name: 'Twitter', icon: 'twitter' },
+  ];
+  $scope.listItemClick = function($index) {
+    var clickedItem = $scope.items[$index];
+    $mdBottomSheet.hide(clickedItem);
+  };
+})
+.run(function($http, $templateCache) {
+    var urls = [
+      'img/icons/share-arrow.svg',
+      'img/icons/upload.svg',
+      'img/icons/copy.svg',
+      'img/icons/print.svg',
+      'img/icons/hangout.svg',
+      'img/icons/mail.svg',
+      'img/icons/message.svg',
+      'img/icons/copy2.svg',
+      'img/icons/facebook.svg',
+      'img/icons/twitter.svg'
+    ];
+    angular.forEach(urls, function(url) {
+      $http.get(url, {cache: $templateCache});
+    });
+  });
